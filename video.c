8591f406b123f9f2a955b228484dad5192931a1e
ghiles <ghiloux03@gmail.com>
2024-11-20 14:47:49 +0100

    implementation de la fonction finale demandée video()

--- video.c
+++ video.c
@@ -10,7 +10,38 @@
 //   colored video size (based on the unit passed parametter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
+   float resultatOctets = (w * h *  1 * fps) *durationCredits + (w * h * 3 * fps) * (durationMovie); // la section en noir et blanc ne concerne que les credits
 
+   // Vérification de l'unité choisie
+   if (unit == NULL)
+   {
+      return -1; // Retourne une valeur d'erreur pour une unité invalide
+   }
+
+   if (strcmp(unit, "o") == 0) // verfier la chaine introduite dans la fonction
+   {
+      return resultatOctets; // Octets
+   }
+   else if (strcmp(unit, "bt") == 0)
+   {
+      return resultatOctets * 8; // Bits
+   }
+   else if (strcmp(unit, "ko") == 0)
+   {
+      return resultatOctets / 1024; // Kilooctets
+   }
+   else if (strcmp(unit, "mo") == 0)
+   {
+      return resultatOctets / (1024 * 1024); // Mégaoctets
+   }
+   else if (strcmp(unit, "go") == 0)
+   {
+      return resultatOctets / (1024 * 1024 * 1024); // Gigaoctets
+   }
+   else
+   {
+      return -1; // Retourne une valeur d'erreur pour une unité non reconnue
+   }
    // YOUR CODE HERE - END
    return 0;
 }
