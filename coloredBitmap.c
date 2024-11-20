bce0e8fdc10594763c686e1b75b61b1d37da2049
ghiles <ghiloux03@gmail.com>
2024-11-20 14:31:59 +0100

    implementation de la fonction coloredBitmap

--- coloredBitmap.c
+++ coloredBitmap.c
@@ -1,14 +1,47 @@
 #include <string.h>
 #include "config/coloredBitmap.h"
+
 // Parameters:
 //   w: width of the image
 //   h: height of the image
 //   u: Unit of the output value. It could be 'bt' bits, 'ko' kilobits, 'mo' megabits, 'go' gigabits
-// Return value
-//   colored image size Bitmap (based on the unit passed parametter)
-float coloredBitmap(int w, int h, char* unit) {
+// Return value:
+//   colored image size Bitmap (based on the unit passed parameter)
+float coloredBitmap(int w, int h, char *unit)
+{
    // YOUR CODE HERE - BEGIN
+   float resultatOctets = w * h * 3;
 
+   // Vérification de l'unité choisie
+   if (unit == NULL)
+   {
+      return -1; // Retourne une valeur d'erreur pour une unité invalide
+   }
+
+   if (strcmp(unit, "o") == 0)
+   {
+      return resultatOctets; // Octets
+   }
+   else if (strcmp(unit, "bt") == 0)
+   {
+      return resultatOctets * 8; // Bits
+   }
+   else if (strcmp(unit, "ko") == 0)
+   {
+      return resultatOctets / 1024; // Kilooctets
+   }
+   else if (strcmp(unit, "mo") == 0)
+   {
+      return resultatOctets / (1024 * 1024); // Mégaoctets
+   }
+   else if (strcmp(unit, "go") == 0)
+   {
+      return resultatOctets / (1024 * 1024 * 1024); // Gigaoctets
+   }
+   else
+   {
+      return -1; // Retourne une valeur d'erreur pour une unité non reconnue
+   }
    // YOUR CODE HERE - END
    return 0;
 }
